e929e468a9b5bf43f5e32fd127559c8cba45dae2
xXboob1Xx <aleksadar2007@gmail.com>
2024-08-23 20:07:54 +0300

    walk and look reak

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@onready var head = $head
 
 var current_speed = 5.0
 
@@ -9,10 +10,17 @@ const crouch_speed = 3.0
 
 const jump_vel = 4.5
 
+const mouse_sens = 0.4
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("sprint"):
